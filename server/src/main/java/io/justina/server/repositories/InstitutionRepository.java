fce596e3b6da2b6f6d878b3c6ad84726bad42408
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-07-18 20:48:15 -0300

    add: Interfaz Repository and Interfaz Service to Institution

---
+++ server/src/main/java/io/justina/server/repositories/InstitutionRepository.java
@@ -0,0 +1,9 @@
+package io.justina.server.repositories;
+
+import io.justina.server.entities.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+}
