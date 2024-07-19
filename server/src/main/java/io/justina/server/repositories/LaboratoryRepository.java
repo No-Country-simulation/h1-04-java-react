b75b718a286b2bfa13fb526f60f64f6e89abdeba
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-07-19 13:22:38 -0300

    add: LaboratoryService

--- server/src/main/java/io/justina/server/repositories/LaboratoryRepository.java
+++ server/src/main/java/io/justina/server/repositories/LaboratoryRepository.java
@@ -2,6 +2,8 @@ package io.justina.server.repositories;
 
 import io.justina.server.entities.Laboratory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LaboratoryRepository extends JpaRepository<Laboratory, Long> {
 }
---
+++ server/src/main/java/io/justina/server/services/LaboratoryService.java
@@ -0,0 +1,21 @@
+package io.justina.server.services;
+
+import io.justina.server.dtos.request.LaboratoryRequestDTO;
+import io.justina.server.dtos.response.LaboratoryResponseDTO;
+import java.util.List;
+
+public interface LaboratoryService {
+
+    LaboratoryResponseDTO createLaboratory(LaboratoryRequestDTO laboratoryRequestDTO);
+
+    LaboratoryResponseDTO getLaboratoryById(Long id);
+
+    List<LaboratoryResponseDTO> getAllLaboratories();
+
+    LaboratoryResponseDTO updateLaboratory(Long id, LaboratoryRequestDTO laboratoryRequestDTO);
+
+    void deleteLaboratory(Long id);
+
+    void deactivateLaboratory(Long id);
+    
+}
