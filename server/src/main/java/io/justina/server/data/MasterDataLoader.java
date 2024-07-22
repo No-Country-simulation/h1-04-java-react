1eb4e52277a576f80620478b910bb7f27ad8d7ff
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-07-22 13:58:58 -0300

    data loader 2 ok

--- server/src/main/java/io/justina/server/data/MasterDataLoader.java
+++ server/src/main/java/io/justina/server/data/MasterDataLoader.java
@@ -9,11 +9,14 @@ public class MasterDataLoader implements CommandLineRunner {
 
     @Autowired
     private DataLoaderRole dataLoaderRole;
+    @Autowired
+    private DataLoaderInstitution dataLoaderInstitution;
     // Añadir más Data Loaders aquí
 
     @Override
     public void run(String... args) throws Exception {
         dataLoaderRole.run(args);
+        dataLoaderInstitution.run(args);
         // Ejecutar más Data Loaders aquí
     }
 
