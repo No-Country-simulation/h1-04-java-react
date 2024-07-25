f754ed4b2289f488d0ce319268bf7b95e4452705
unknown <marcofacundolucas@gmail.com>
2024-07-25 14:34:37 -0300

    page completion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,12 @@ import Router from "./Router/Routers.jsx";
 import { useContext } from "react";
 import DoctorContext from "./context/DoctorContext.jsx";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const { authData } = useContext(DoctorContext);
   console.log(authData);
-
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <Router />
   );
 }
 
