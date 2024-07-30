83bf228f1eefb683ee3c8eb8440bae04de0c8513
CamiloProg <camilom200107@gmail.com>
2024-07-30 14:01:00 -0500

    turn-calendar style

--- client/src/Components/Modals/SucessModal.jsx
+++ client/src/Components/Modals/SucessModal.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import checkimg from "../../Assets/Imgs/checkIMG.svg";
 
 const VerificandoModal = ({
   show,
   onClose,
   title = "VERIFICANDO TU TURNO",
   text = "Aguárdanos, estamos verificando y confirmando tu turno.",
+  check,
 }) => {
   if (!show) return null;
 
@@ -19,14 +21,20 @@ const VerificandoModal = ({
       className='fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50'
       onClick={handleBackgroundClick}
     >
-      <div className='bg-white rounded-lg p-6 w-80'>
+      <div className='bg-white border-2 h-80 flex justify-center items-center border-secondary rounded-lg p-6 w-80'>
         <div className='text-center'>
-          <div className='mb-4'>
-            <span role='img' aria-label='Verificando' className='text-4xl'>
-              ❤️
-            </span>
+          <div className='mb-4 flex justify-center'>
+            {check ? (
+              <div className='bg-secondary rounded-full w-20 h-20 flex justify-center items-center '>
+                <img className='p-4' src={checkimg} alt='' />
+              </div>
+            ) : (
+              <span role='img' aria-label='Verificando' className='text-4xl'>
+                ❤️
+              </span>
+            )}
           </div>
-          <p className='text-lg font-semibold'>{title}</p>
+          <p className='text-lg font-semibold text-secondary'>{title}</p>
           <p className='text-gray-600 mt-2'>{text}</p>
         </div>
       </div>
