4246e011abf81760c648c78f254133cfcd38ecd4
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-07-31 00:58:35 -0300

    fix: setting form in admin panel

--- client/src/Components/Admin/EditPatientForm.jsx
+++ client/src/Components/Admin/EditPatientForm.jsx
@@ -1,12 +1,319 @@
+import { useState } from 'react';
+
 const EditPatientForm = ({ patient, onClose }) => {
+  const [formData, setFormData] = useState(patient);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const isFieldEmpty = (field) => !formData[field];
+
   return (
     <div>
-      <h2 className="text-lg font-semibold mb-4">Editar Paciente</h2>
+      <h2 className="text-lg font-semibold mb-4 text-center">Editar Paciente</h2>
       <form>
-        {/* Completa este formulario con los campos necesarios para editar un paciente */}
-        <button type="submit" className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border borde">Guardar</button>
+        <div className="grid grid-cols-3 gap-4">
+          <input
+            type="text"
+            name="nombre"
+            placeholder="Nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+            className={`border p-1 text-sm rounded ${isFieldEmpty('nombre') ? 'border-red-500' : 'border-blue-500'}`}
+          />
+          <input
+            type="text"
+            name="apellido"
+            placeholder="Apellido"
+            value={formData.apellido}
+            onChange={handleChange}
+            className={`border p-1 text-sm rounded ${isFieldEmpty('apellido') ? 'border-red-500' : 'border-blue-500'}`}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            className={`border p-1 text-sm rounded ${isFieldEmpty('email') ? 'border-red-500' : 'border-blue-500'}`}
+          />
+          <select
+            name="dni"
+            value={formData.dni}
+            onChange={handleChange}
+            className={`border p-1 text-sm rounded ${isFieldEmpty('dni') ? 'border-red-500' : 'border-blue-500'}`}
+          >
+            <option value="">Seleccione DNI</option>
+            <option value="DNI">DNI</option>
+            <option value="LC">LC</option>
+            <option value="LE">LE</option>
+          </select>
+          <input
+            type="text"
+            name="nroDocumento"
+            placeholder="Nro Documento"
+            maxLength="8"
+            value={formData.nroDocumento}
+            onChange={handleChange}
+            className={`border p-1 text-sm rounded ${isFieldEmpty('nroDocumento') ? 'border-red-500' : 'border-blue-500'}`}
+          />
+          <select
+            name="obraSocial"
+            value={formData.obraSocial}
+            onChange={handleChange}
+            className={`border p-1 text-sm rounded ${isFieldEmpty('obraSocial') ? 'border-red-500' : 'border-blue-500'}`}
+          >
+            <option value="">Seleccione Obra Social</option>
+            <option value="OSDE">OSDE</option>
+            <option value="Medicus">Medicus</option>
+            <option value="Swiss Medical">Swiss Medical</option>
+            <option value="PAMI">PAMI</option>
+            <option value="No Tiene">No Tiene</option>
+          </select>
+          <input
+            type="text"
+            name="telefono"
+            placeholder="Teléfono"
+            value={formData.telefono || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+          <input
+            type="text"
+            name="calle"
+            placeholder="Calle / Manzana"
+            value={formData.calle || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+          <input
+            type="text"
+            name="numero"
+            placeholder="Número / Lote"
+            value={formData.numero || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+          <input
+            type="date"
+            name="fechaNacimiento"
+            placeholder="Fecha Nacimiento"
+            value={formData.fechaNacimiento || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+          <input
+            type="text"
+            name="barrio"
+            placeholder="Barrio"
+            value={formData.barrio || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+          <input
+            type="text"
+            name="ciudad"
+            placeholder="Ciudad"
+            value={formData.ciudad || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+          <select
+            name="grupoSanguineo"
+            value={formData.grupoSanguineo || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          >
+            <option value="">Seleccione Grupo Sanguíneo</option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+          </select>
+          <input
+            type="text"
+            name="provincia"
+            placeholder="Provincia"
+            value={formData.provincia || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+          <input
+            type="text"
+            name="codigoPostal"
+            placeholder="Código Postal"
+            value={formData.codigoPostal || ''}
+            onChange={handleChange}
+            className="border p-1 text-sm rounded"
+          />
+
+          <div className="col-span-1 col-start-1">
+            <label className="block mb-2 text-sm">¿Tiene alguna patología?</label>
+            <div className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="patologias"
+                value="Sí"
+                checked={formData.patologias === 'Sí'}
+                onChange={handleChange}
+                className="mr-2"
+              /> Sí
+              <input
+                type="radio"
+                name="patologias"
+                value="No"
+                checked={formData.patologias === 'No'}
+                onChange={handleChange}
+                className="ml-4 mr-2"
+              /> No
+            </div>
+            <textarea
+              name="descripcionPatologias"
+              placeholder="Describa las patologías"
+              value={formData.descripcionPatologias || ''}
+              onChange={handleChange}
+              className="border p-1 text-sm rounded w-full h-[4.2rem]"
+            ></textarea>
+          </div>
+
+          <div className="col-span-1 col-start-2">
+            <label className="block mb-2 text-sm">¿Tiene Tratamientos Activos?</label>
+            <div className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="tratamientos"
+                value="Sí"
+                checked={formData.tratamientos === 'Sí'}
+                onChange={handleChange}
+                className="mr-2"
+              /> Sí
+              <input
+                type="radio"
+                name="tratamientos"
+                value="No"
+                checked={formData.tratamientos === 'No'}
+                onChange={handleChange}
+                className="ml-4 mr-2"
+              /> No
+            </div>
+            <textarea
+              name="descripcionTratamientos"
+              placeholder="Describa los tratamientos"
+              value={formData.descripcionTratamientos || ''}
+              onChange={handleChange}
+              className="border p-1 text-sm rounded w-full h-[4.2rem]"
+            ></textarea>
+          </div>
+
+          <div className="col-span-1 col-start-3">
+            <label className="block mb-2 text-sm">¿Tiene Tutor o encargado?</label>
+            <div className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="tutor"
+                value="Sí"
+                checked={formData.tutor === 'Sí'}
+                onChange={handleChange}
+                className="mr-2"
+              /> Sí
+              <input
+                type="radio"
+                name="tutor"
+                value="No"
+                checked={formData.tutor === 'No'}
+                onChange={handleChange}
+                className="ml-4 mr-2"
+              /> No
+            </div>
+            <input
+              type="text"
+              name="nombreTutor"
+              placeholder="Nombre Completo Tutor"
+              value={formData.nombreTutor || ''}
+              onChange={handleChange}
+              className="border p-1 text-sm rounded w-full mb-2"
+            />
+            <input
+              type="text"
+              name="telefonoTutor"
+              placeholder="Teléfono Tutor"
+              value={formData.telefonoTutor || ''}
+              onChange={handleChange}
+              className="border p-1 text-sm rounded w-full"
+            />
+          </div>
+
+          <div className="col-span-1 col-start-1">
+            <label className="block mb-2 text-sm">¿Tiene Transplante?</label>
+            <div className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="transplante"
+                value="Sí"
+                checked={formData.transplante === 'Sí'}
+                onChange={handleChange}
+                className="mr-2"
+              /> Sí
+              <input
+                type="radio"
+                name="transplante"
+                value="No"
+                checked={formData.transplante === 'No'}
+                onChange={handleChange}
+                className="ml-4 mr-2"
+              /> No
+            </div>
+            <textarea
+              name="descripcionTransplante"
+              placeholder="Describa los transplantes"
+              value={formData.descripcionTransplante || ''}
+              onChange={handleChange}
+              className="border p-1 text-sm rounded w-full h-[4.2rem]"
+            ></textarea>
+          </div>
+
+          <div className="col-span-1 col-start-2">
+            <label className="block mb-2 text-sm">¿Está tomando alguna medicación?</label>
+            <div className="flex items-center mb-2">
+              <input
+                type="radio"
+                name="medicacion"
+                value="Sí"
+                checked={formData.medicacion === 'Sí'}
+                onChange={handleChange}
+                className="mr-2"
+              /> Sí
+              <input
+                type="radio"
+                name="medicacion"
+                value="No"
+                checked={formData.medicacion === 'No'}
+                onChange={handleChange}
+                className="ml-4 mr-2"
+              /> No
+            </div>
+            <textarea
+              name="descripcionMedicacion"
+              placeholder="Describa las medicaciones"
+              value={formData.descripcionMedicacion || ''}
+              onChange={handleChange}
+              className="border p-1 text-sm rounded w-full h-[4.2rem]"
+            ></textarea>
+          </div>
+
+          <div className="flex flex-col items-center justify-evenly col-span-1 col-start-3">
+            <h4 className='pb-2 text-md font-base text-red-600'>*Los campos en rojo son obligatorios.</h4>
+            <button type="submit" className="w-52 bg-blue-300 hover:bg-[#48c2ff] text-[#143b50] font-bold py-2 px-4 rounded border border-[#246183]">Guardar</button>
+            <button onClick={onClose} className="w-52 bg-red-400 hover:bg-red-600 text-red-800 hover:text-white font-bold py-2 px-4 rounded border border-[#7c232b]">Cerrar</button>
+          </div>
+        </div>
       </form>
-      <button onClick={onClose} className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded border border-[#e4626f]">Cerrar</button>
     </div>
   );
 }
--- client/src/Components/Admin/Settings.jsx
+++ client/src/Components/Admin/Settings.jsx
@@ -32,172 +32,171 @@ const Settings = () => {
   };
 
   return (
-    <div className="p-4 font-roboto">
-      <h2 className="text-xl font-semibold mb-8">Configuración</h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="p-2 font-roboto text-sm">
+      <h2 className="text-lg font-semibold mb-4">Configuración</h2>
+      <form onSubmit={handleSubmit} className="space-y-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
           <div>
-            <label htmlFor="nombre" className="block text-lg font-medium text-gray-700">Nombre</label>
+            <label htmlFor="nombre" className="block text-base font-medium text-gray-700">Nombre</label>
             <input
               type="text"
               id="nombre"
               name="nombre"
               value={adminData.nombre}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="apellido" className="block text-lg font-medium text-gray-700">Apellido</label>
+            <label htmlFor="apellido" className="block text-base font-medium text-gray-700">Apellido</label>
             <input
               type="text"
               id="apellido"
               name="apellido"
               value={adminData.apellido}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="email" className="block text-lg font-medium text-gray-700">Email</label>
+            <label htmlFor="email" className="block text-base font-medium text-gray-700">Email</label>
             <input
               type="email"
               id="email"
               name="email"
               value={adminData.email}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="telefono" className="block text-lg font-medium text-gray-700">Teléfono</label>
+            <label htmlFor="telefono" className="block text-base font-medium text-gray-700">Teléfono</label>
             <input
               type="tel"
               id="telefono"
               name="telefono"
               value={adminData.telefono}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="fechaNacimiento" className="block text-lg font-medium text-gray-700">Fecha de Nacimiento</label>
+            <label htmlFor="fechaNacimiento" className="block text-base font-medium text-gray-700">Fecha de Nacimiento</label>
             <input
               type="date"
               id="fechaNacimiento"
               name="fechaNacimiento"
               value={adminData.fechaNacimiento}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="tipoDocumento" className="block text-lg font-medium text-gray-700">Tipo de Documento</label>
+            <label htmlFor="tipoDocumento" className="block text-base font-medium text-gray-700">Tipo de Documento</label>
             <input
               type="text"
               id="tipoDocumento"
               name="tipoDocumento"
               value={adminData.tipoDocumento}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="nroDocumento" className="block text-lg font-medium text-gray-700">Nro Documento</label>
+            <label htmlFor="nroDocumento" className="block text-base font-medium text-gray-700">Nro Documento</label>
             <input
               type="text"
               id="nroDocumento"
               name="nroDocumento"
               value={adminData.nroDocumento}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
           <div>
-            <label htmlFor="calle" className="block text-lg font-medium text-gray-700">Calle</label>
+            <label htmlFor="calle" className="block text-base font-medium text-gray-700">Calle</label>
             <input
               type="text"
               id="calle"
               name="calle"
               value={adminData.calle}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="numero" className="block text-lg font-medium text-gray-700">Número</label>
+            <label htmlFor="numero" className="block text-base font-medium text-gray-700">Número</label>
             <input
               type="text"
               id="numero"
               name="numero"
               value={adminData.numero}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="barrio" className="block text-lg font-medium text-gray-700">Barrio</label>
+            <label htmlFor="barrio" className="block text-base font-medium text-gray-700">Barrio</label>
             <input
               type="text"
               id="barrio"
               name="barrio"
               value={adminData.barrio}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="ciudad" className="block text-lg font-medium text-gray-700">Ciudad</label>
+            <label htmlFor="ciudad" className="block text-base font-medium text-gray-700">Ciudad</label>
             <input
               type="text"
               id="ciudad"
               name="ciudad"
               value={adminData.ciudad}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="provincia" className="block text-lg font-medium text-gray-700">Provincia</label>
+            <label htmlFor="provincia" className="block text-base font-medium text-gray-700">Provincia</label>
             <input
               type="text"
               id="provincia"
               name="provincia"
               value={adminData.provincia}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
           <div>
-            <label htmlFor="codigoPostal" className="block text-lg font-medium text-gray-700">Código Postal</label>
+            <label htmlFor="codigoPostal" className="block text-base font-medium text-gray-700">Código Postal</label>
             <input
               type="text"
               id="codigoPostal"
               name="codigoPostal"
               value={adminData.codigoPostal}
               onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+              className="mt-1 p-1 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
             />
           </div>
         </div>
-        <div className='flex gap-4'>
+        <div className='flex gap-2'>
           <button
             type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded-md transition-colors duration-300 hover:bg-blue-600"
+            className="bg-blue-500 text-white px-2 py-1 rounded-md transition-colors duration-300 hover:bg-blue-600"
           >
             Actualizar Datos
           </button>
           <button
-            className="bg-red-500 text-white px-4 py-2 rounded-md transition-colors duration-300 hover:bg-red-600"
+            className="bg-red-500 text-white px-2 py-1 rounded-md transition-colors duration-300 hover:bg-red-600"
           >
             Actualizar Password
           </button>
         </div>
-
       </form>
-      <div className="mt-8">
+      <div className="mt-4">
   
       </div>
     </div>
