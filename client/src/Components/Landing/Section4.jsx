f1b3b8a7748f5873b3def071a2bba4ffe3d159b1
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-08-02 20:13:04 -0300

    fix: section4 landing

--- client/src/Components/Landing/Section4.jsx
+++ client/src/Components/Landing/Section4.jsx
@@ -1,68 +1,39 @@
-import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
 import img1 from '../../Assets/Imgs/section4img1.png';
 import img2 from '../../Assets/Imgs/section4img2.png';
 import img3 from '../../Assets/Imgs/section4img3.png';
 
-// Imágenes para el carrusel (reemplaza estas URL con tus propias imágenes)
-const images = [
-  img1,
-  img2,
-  img3,
-  img1,
-  img2,
-  img3
-];
-
 const Section4 = () => {
   return (
-    <div className="w-full h-full bg-[#fdeee5] flex">
+    <div className="w-full h-[34.75rem] px-[5rem] bg-[#fdeee5] flex justify-between items-center">
       {/* Columna 1 */}
-      <div className="w-5/12 flex flex-col justify-center items-start px-12 py-12 pl-24">
-        <div className="text-[#ea526f] text-[40px] font-semibold leading-[44px] mb-6">
-          Sobre Justina Lo Cane
+      <div className=" flex justify-center items-center">
+        <div className="flex flex-col justify-start items-start gap-8">
+          <div className="text-[#ea526f] text-[2.5rem] font-semibold leading-[2.75rem]">Sobre Justina Lo Cane</div>
+          <div className="w-full text-[#555555] text-base font-medium leading-tight">
+            Justina Lo Cane tenía un año y medio cuando recibió junto a sus padres la noticia de que padecía una cardiopatía congénita. A partir de ese momento, con controles y medicamentos, logró llevar adelante una vida normal hasta que a los 12 años, el cuadro se complicó y la llevó a la necesidad de un trasplante de corazón.
+          </div>
+          <a 
+            href="https://www.facebook.com/multiplicatex7/" 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-[8.4375rem] h-[2.5625rem] p-3 bg-[#ea526f] rounded-lg flex justify-center items-center gap-2 transition-colors duration-300 hover:bg-[#d84e62] hover:text-[#ffffff]"
+          >
+            <div className="text-white text-sm font-normal">Más Información</div>
+          </a>
         </div>
-        <div className="w-[28.8rem] text-[#555555] text-base font-semibold leading-tight mb-6 py-3">
-          Justina Lo Cane tenía un año y medio cuando recibió junto a sus padres la noticia de que padecía una cardiopatía congénita. A partir de ese momento, con controles y medicamentos, logró llevar adelante una vida normal hasta que a los 12 años, el cuadro se complicó y la llevó a la necesidad de un trasplante de corazón.
-        </div>
-        <a 
-          href="https://www.facebook.com/multiplicatex7/"  // Reemplaza con la URL deseada
-          target="_blank"
-          rel="noopener noreferrer"
-          className="w-[135px] h-[41px] p-3 bg-[#ea526f] rounded-lg flex justify-center items-center transition-colors duration-300 hover:bg-[#d84e62] hover:text-[#ffffff]"
-        >
-          <div className="text-white text-sm font-normal leading-[17px] transition-colors duration-300 hover:text-[#ffffff]">Más Información</div>
-        </a>
       </div>
-
-      {/* Columna 2 */}
-      <div className="w-7/12 relative">
-        <CarouselProvider
-          naturalSlideWidth={100}
-          naturalSlideHeight={125}
-          totalSlides={images.length}
-          visibleSlides={2}
-          interval={3000}  // Intervalo de 3 segundos
-          isPlaying={true}  // Habilitar el avance automático
-          className="h-[35rem]"
-        >
-          <Slider className="h-[90%] py-14 my-1 px-10">
-            {images.map((image, index) => (
-              <Slide key={index}>
-                  <div className="flex flex-col justify-start items-center gap-3">
-                    <div className="w-[20rem] h-full">  {/* Ajuste de ancho */}
-                      <img
-                        className="w-[20rem] h-full object-cover absolute "
-                        src={image}  // Usa `image` en lugar de `images`
-                        alt={`image${index + 1}`}
-                      />
-                    </div>
-                  </div>
-              </Slide>
-            ))}
-          </Slider>
-        </CarouselProvider>
-        <div className="absolute inset-y-0 right-0 w-40 bg-gradient-to-l from-[#fdeee5] to-transparent" />
+      
+      {/* Columna 2: Imágenes */}
+      <div className="flex flex-row justify-center items-center gap-4">
+        <div className="w-[15.625rem] h-[24.8125rem] relative">
+          <img className="w-[15.625rem] h-[24.8125rem] " src={img1} alt="Imagen 1" />
+        </div>
+        <div className="w-[15.625rem] h-[24.8125rem] relative">
+          <img className="w-[15.625rem] h-[24.8125rem] " src={img2} alt="Imagen 2" />
+        </div>
+        <div className="w-[15.625rem] h-[24.8125rem] relative">
+          <img className="w-[15.625rem] h-[24.8125rem] " src={img3} alt="Imagen 3" />
+        </div>
       </div>
     </div>
   );
