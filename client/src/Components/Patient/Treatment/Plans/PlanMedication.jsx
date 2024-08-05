384acdd445b50cc25cadfcd9e01541e91170db85
CamiloProg <camilom200107@gmail.com>
2024-08-04 22:47:48 -0500

    medication

--- client/src/Components/Patient/Treatment/Plans/PlanMedication.jsx
+++ client/src/Components/Patient/Treatment/Plans/PlanMedication.jsx
@@ -1,98 +1,112 @@
-import PropTypes from "prop-types"
-import { useState } from "react";
+import PropTypes from "prop-types";
+import { useContext, useEffect, useState } from "react";
+import arrowRight from "../../../../Assets/Imgs/arrowRight.png";
+import undo from "../../../../Assets/Imgs/undo.png";
+import "./plans.css";
 import arrowOrange from "../../../../Assets/Imgs/arrowOrange.svg";
-import azatioprina from "../../../../Assets/Imgs/azatioprina.png"
-import muyBien from "../../../../Assets/Imgs/muyBien.png"
-import normal from "../../../../Assets/Imgs/normal.png"
-import mal from "../../../../Assets/Imgs/mal.png"
-import "./plans.css"
-
-const optionsTreat = [
-    { label: "Azatioprima" },
-    { label: "Clonazepan" },
-    { label: "Ibuprofeno" }
-];
-
+import azatioprina from "../../../../Assets/Imgs/azatioprina.png";
+import muyBien from "../../../../Assets/Imgs/muyBien.png";
+import normal from "../../../../Assets/Imgs/normal.png";
+import mal from "../../../../Assets/Imgs/mal.png";
+import DoctorContext from "../../../../context/DoctorContext";
 
 const PlanMedication = () => {
-    const [isOpen, setIsOpen] = useState(Array(optionsTreat.length).fill(false));
+  const [isOpen, setIsOpen] = useState([]);
+  const { fetchPatientById, authData } = useContext(DoctorContext);
+  const [patientData, setPatientData] = useState([]);
+  const [error, setError] = useState(null);
 
-    const toggleDropdown = (index) => {
-        setIsOpen(prevState => {
-            const newState = [...prevState];
-            newState[index] = !newState[index];
-            return newState;
-        });
+  useEffect(() => {
+    const loadPatientData = async () => {
+      if (authData) {
+        try {
+          const data = await fetchPatientById(authData.id);
+          setPatientData(data.patient.treatments);
+          setIsOpen(Array(data.patient.treatments.length).fill(false));
+          setError(null);
+        } catch (error) {
+          setError(error.message);
+          setPatientData([]);
+        }
+      }
     };
 
+    loadPatientData();
+  }, [authData, fetchPatientById]);
 
-    return (
-        <article>
-            { optionsTreat.map((comp, index) => (
-                <div key={index}>
-                    <button className="option font-bold flex justify-between p-2" onClick={() => toggleDropdown(index)}>
-                        {comp.label}
-                        <img src={arrowOrange} alt="arrow" className={`w-4 h-6 ml-4 ${isOpen[index] ? 'more-more-arrow-rotate' : 'more-arrow-rotate'}`} />
-                    </button>
-                    { isOpen[index] && (
-                        <>
-                        <div className="containerMedication">
-                            <div className="undo">
-                                <img src={azatioprina} alt="undo" />
-                                <div>
-                                    <h1>{comp.label}</h1>
-                                    <p>Micofenolato-mofetil | Micofenolato sódico</p>
-                                </div>
-                            </div>
-                            
-                            <section className="flex justify-center text-center">
-                                <div>
-                                    <p>Frecuencia</p>
-                                    <span>8</span>
-                                    <p>horas</p>
-                                </div>
-                                <div>
-                                    <p>Dosis</p>
-                                    <span>1</span>
-                                    <p>dosis</p>
-                                </div>
-                                <div>
-                                    <p>Duración</p>
-                                    <span>21</span>
-                                    <p>días</p>
-                                </div>
-                            </section>
-                            
-                            <span className="font-bold">Instrucciones</span>
-                            <p>Cada 8 horas debe tomar una dosis, luego de la comida. </p>
-                            <div className="buttonOk">
-                                <button onClick={() => alert("Descargando...")}>Descargar Receta</button>
-                            </div>
-                        </div>
-                        <section className="feedBack">
-                            <h3>¿Cómo te sentiste con la comida?</h3>
-                            <div className="feedBackImgs">
-                                <img src={muyBien} alt="Muy bien" />
-                                <img src={normal} alt="Normal" />
-                                <img src={mal} alt="Mal" />
-                            </div>
-                            <p>¿Por qué te sentiste así?</p>
-                            <textarea name="text" id="text"></textarea>
-                            <div className="feedBackButtons">
-                                <button>Cancelar</button>
-                                <button>Confirmar</button>
-                            </div>
-                        </section>
-                        </>
-                    ) }
-                </div>
-            )) }
-        </article>
-    )
-}
+  const toggleDropdown = (index) => {
+    setIsOpen((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  return (
+    <article>
+      {patientData.length > 0 ? (
+        patientData.map((treatment, index) => (
+          <div key={index}>
+            <button
+              className='option bg-beigeColor font-bold flex justify-between p-2'
+              onClick={() => toggleDropdown(index)}
+            >
+              {treatment.treatmentName}
+              <img
+                src={arrowRight}
+                alt='arrow'
+                className={`w-6 h-6 ml-4 ${
+                  isOpen[index] ? "arrow-rotate" : ""
+                }`}
+              />
+            </button>
+            {isOpen[index] && (
+              <div className='containerMedication'>
+                {treatment.medicalPrescriptions.map((prescription) => (
+                  <div
+                    key={prescription.id}
+                    className='flex flex-col border-b-4 mb-4'
+                  >
+                    <div className='flex items-center gap-5 font-bold text-orangeColor'>
+                      <img src={azatioprina} alt='Muy bien' />
+
+                      <h1>{prescription.medication.medicationName}</h1>
+                    </div>
+
+                    <section className='flex justify-center text-center'>
+                      <div>
+                        <p>Frecuencia</p>
+                        <p>{prescription.doseFrequency}</p>
+                      </div>
+                      <div>
+                        <p>Dosis</p>
+                        <p>{prescription.doseSize}</p>
+                      </div>
+                    </section>
+
+                    <span className='font-bold'>Instrucciones</span>
+                    <p>{prescription.indications}</p>
+                    <div className='buttonOk mb-5'>
+                      <button onClick={() => alert("Descargando...")}>
+                        Descargar Receta
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))
+      ) : (
+        <p>No hay tratamientos disponibles</p>
+      )}
+      {error && <p>Error: {error}</p>}
+    </article>
+  );
+};
 
 PlanMedication.propTypes = {
-    type: PropTypes.string.isRequired
-}
+  type: PropTypes.string.isRequired,
+};
 
-export default PlanMedication
+export default PlanMedication;
--- client/src/Components/Patient/Treatment/Treatment.jsx
+++ client/src/Components/Patient/Treatment/Treatment.jsx
@@ -1,7 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import { treatmentData } from "./data.js"
+import { treatmentData } from "./data.js";
 import { useState } from "react";
-import anotherArrowLeft from "../../../Assets/Imgs/otraArrowLeft.png"
+import anotherArrowLeft from "../../../Assets/Imgs/otraArrowLeft.png";
 import PlanTreatment from "./Plans/PlanTreatment.jsx";
 import PlanStudies from "./Plans/PlanStudies.jsx";
 import PlanNutrition from "./Plans/PlanNutrition/PlanNutrition.jsx";
@@ -12,75 +12,88 @@ import PlanPsychology from "./Plans/PlanPsychology.jsx";
 import PlanSocial from "./Plans/PlanSocial.jsx";
 import PlanPhysiotherapy from "./Plans/PlanPhysiotherapy.jsx";
 import PlanOthers from "./Plans/PlanOthers.jsx";
-import download from "../../../Assets/Imgs/download.png"
+import download from "../../../Assets/Imgs/download.png";
 import arrowOrange from "../../../Assets/Imgs/arrowOrange.svg";
-import "./treatment.css"
-
+import "./treatment.css";
 
 const componentsMap = {
-    '/treatment-treatment': PlanTreatment,
-    '/treatment-studies': PlanStudies,
-    '/treatment-nutrition': PlanNutrition,
-    '/treatment-physical-activity': PlanGim,
-    '/treatment-medication': PlanMedication,
-    '/treatment-clinical-history': PlanClinical,
-    '/treatment-psychology': PlanPsychology,
-    '/treatment-social-work': PlanSocial,
-    '/treatment-physiotherapy': PlanPhysiotherapy,
-    '/treatment-others': PlanOthers,
+  "/treatment-treatment": PlanTreatment,
+  "/treatment-studies": PlanStudies,
+  "/treatment-nutrition": PlanNutrition,
+  "/treatment-physical-activity": PlanGim,
+  "/treatment-clinical-history": PlanClinical,
+  "/treatment-psychology": PlanPsychology,
+  "/treatment-social-work": PlanSocial,
+  "/treatment-physiotherapy": PlanPhysiotherapy,
+  "/treatment-others": PlanOthers,
 };
 
-
 const Treatment = () => {
-    const location = useLocation();
-    const navigate = useNavigate()
-    const { image, title, buttons } = treatmentData[location.pathname] || {};
-    const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { image, title, buttons } = treatmentData[location.pathname] || {};
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
 
-    const handleComponent = (index) => {
-        setSelectedButtonIndex(index);
-    };
+  const handleComponent = (index) => {
+    setSelectedButtonIndex(index);
+  };
 
-    const SelectedComponent = selectedButtonIndex !== null ? componentsMap[location.pathname] : null;
-    const selectedButton = selectedButtonIndex !== null ? buttons[selectedButtonIndex] : null;
+  const SelectedComponent =
+    selectedButtonIndex !== null &&
+    location.pathname !== "/treatment-medication"
+      ? componentsMap[location.pathname]
+      : null;
 
+  const selectedButton =
+    selectedButtonIndex !== null ? buttons[selectedButtonIndex] : null;
 
-    return (
-        <section className="container">
-            <div className="titleContainer">
-                {image && <img src={image} alt={title} />}
-                <h1>{title}</h1>
-            </div>
-            <article className="planes">
-                <div>
-                    <h2>Plan de {title}</h2>
-                    <p>Plan personalizado</p>
-                </div>
-                <img src={download} alt="download" /> {/* Descargar PDF o algo */}
-            </article>
-            { !SelectedComponent ? (
-                <>
-                <article className="optionsContainer">
-                    { buttons && buttons.map((but, index) => (
-                        <button key={index} className="option font-bold flex justify-between p-2" onClick={() => handleComponent(index)}>
-                            { but.label }
-                            <img src={arrowOrange} alt="arrow" className="w-4 h-6 ml-4 more-arrow-rotate" />
-                        </button>
-                    )) }
-                </article>
-                </>
-            ) : (
-                <article className="containerComponents">
-                    <SelectedComponent type={selectedButton.label} />
-                </article>
-            ) }
-            <div className="backContainer">
-                <button className="back" onClick={() => navigate(-1)}>
-                    <img src={anotherArrowLeft} alt="back" />
-                </button>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className='container'>
+      <div className='titleContainer'>
+        {image && <img src={image} alt={title} />}
+        <h1>{title}</h1>
+      </div>
+      <article className='planes'>
+        <div>
+          <h2>Plan de {title}</h2>
+          <p>Plan personalizado</p>
+        </div>
+        <img src={download} alt='download' /> {/* Descargar PDF o algo */}
+      </article>
+      {location.pathname !== "/treatment-medication" && !SelectedComponent ? (
+        <article className='optionsContainer'>
+          {buttons &&
+            buttons.map((but, index) => (
+              <button
+                key={index}
+                className='option font-bold flex justify-between p-2'
+                onClick={() => handleComponent(index)}
+              >
+                {but.label}
+                <img
+                  src={arrowOrange}
+                  alt='arrow'
+                  className='w-4 h-6 ml-4 more-arrow-rotate'
+                />
+              </button>
+            ))}
+        </article>
+      ) : (
+        <article className='containerComponents'>
+          {location.pathname === "/treatment-medication" ? (
+            <PlanMedication type={selectedButton?.label} />
+          ) : (
+            <SelectedComponent type={selectedButton?.label} />
+          )}
+        </article>
+      )}
+      <div className='backContainer'>
+        <button className='back' onClick={() => navigate(-1)}>
+          <img src={anotherArrowLeft} alt='back' />
+        </button>
+      </div>
+    </section>
+  );
+};
 
-export default Treatment
+export default Treatment;
