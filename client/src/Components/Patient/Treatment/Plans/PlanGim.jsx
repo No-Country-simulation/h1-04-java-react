fa0aa604347eefd1f14a5a3b857f50b56f8044f6
unknown <marcofacundolucas@gmail.com>
2024-08-05 13:07:54 -0300

    Fix Treatments

--- client/src/Components/Patient/Treatment/Plans/PlanGim.jsx
+++ client/src/Components/Patient/Treatment/Plans/PlanGim.jsx
@@ -7,11 +7,31 @@ import normal from "../../../../Assets/Imgs/normal.png"
 import mal from "../../../../Assets/Imgs/mal.png"
 import imageExample from "../../../../Assets/Imgs/imageExample.png"
 import question from "../../../../Assets/Imgs/question.png"
+import arrowOrange from "../../../../Assets/Imgs/arrowOrange.svg";
 import "./plans.css"
 
+const optionsGim = [
+    { label: "Día Uno" },
+    { label: "Día Dos" },
+    { label: "Día Tres" },
+    { label: "Día Cuatro" },
+    { label: "Día Cinco" },
+    { label: "Extra" },
+    { label: "Comunicación" }
+];
+
 const PlanGim = () => {
+    const [isOpen, setIsOpen] = useState(Array(optionsGim.length).fill(false));
     const [isPopUpVisible, setIsPopUpVisible] = useState(false);
     const [selectedExercise, setSelectedExercise] = useState(null);
+
+    const toggleDropdown = (index) => {
+        setIsOpen(prevState => {
+            const newState = [...prevState];
+            newState[index] = !newState[index];
+            return newState;
+        });
+    };
 
     const exercises = [
         { id: 1, name: "Puente de glúteos con core bag", series: 3, reps: 15, pause: "1:00 m." },
@@ -37,48 +57,59 @@ const PlanGim = () => {
 
     return (
         <article>
-            <div className={`mainContainer ${isPopUpVisible ? "blurred" : ""}`}>
-                <section className="weekContainer">
-                    <Week />
-                </section>
-                
-                <section className="exerciseList">
-                    {exercises.map(exercise => (
-                        <div key={exercise.id} className="exerciseItem">
-                            <p className="exerciseHeader">{exercise.id}</p>
-                            <article className="containeExerciser">
-                                <div className="exerciseContent">
-                                    <h2>{exercise.name}</h2>
-                                    <img src={question} alt="question" onClick={() => {
-                                            setIsPopUpVisible(true);
-                                            setSelectedExercise(exercise.name);
-                                        }} />
+            { optionsGim.map((comp, index) => (
+                <div key={index}>
+                    <button className="option font-bold flex justify-between p-2" onClick={() => toggleDropdown(index)}>
+                        {comp.label}
+                        <img src={arrowOrange} alt="arrow" className={`w-4 h-6 ml-4 ${isOpen[index] ? 'more-more-arrow-rotate' : 'more-arrow-rotate'}`} />
+                    </button>
+                    { isOpen[index] && (
+                        <div className={`mainContainer ${isPopUpVisible ? "blurred" : ""}`}>
+                            <section className="weekContainer">
+                                <Week />
+                            </section>
+                            
+                            <section className="exerciseList">
+                                {exercises.map(exercise => (
+                                    <div key={exercise.id} className="exerciseItem">
+                                        <p className="exerciseHeader">{exercise.id}</p>
+                                        <article className="containeExerciser">
+                                            <div className="exerciseContent">
+                                                <h2>{exercise.name}</h2>
+                                                <img src={question} alt="question" onClick={() => {
+                                                        setIsPopUpVisible(true);
+                                                        setSelectedExercise(exercise.name);
+                                                    }} />
+                                            </div>
+                                            <div className="exerciseDetails">
+                                                <p>Series: {exercise.series}</p>
+                                                <p>Reps: {exercise.reps}</p>
+                                                <p>Pausa: {exercise.pause}</p>
+                                            </div>
+                                        </article>
+                                    </div>
+                                ))}
+                            </section>
+                            
+                            <section className="feedBack">
+                                <h3>¿Cómo te sentiste con la comida?</h3>
+                                <div className="feedBackImgs">
+                                    <img src={muyBien} alt="Muy bien" />
+                                    <img src={normal} alt="Normal" />
+                                    <img src={mal} alt="Mal" />
                                 </div>
-                                <div className="exerciseDetails">
-                                    <p>Series: {exercise.series}</p>
-                                    <p>Reps: {exercise.reps}</p>
-                                    <p>Pausa: {exercise.pause}</p>
+                                <p>¿Por qué te sentiste así?</p>
+                                <textarea name="text" id="text"></textarea>
+                                <div className="feedBackButtons">
+                                    <button>Cancelar</button>
+                                    <button>Confirmar</button>
                                 </div>
-                            </article>
+                            </section>
                         </div>
-                    ))}
-                </section>
-                
-                <section className="feedBack">
-                    <h3>¿Cómo te sentiste con la comida?</h3>
-                    <div className="feedBackImgs">
-                        <img src={muyBien} alt="Muy bien" />
-                        <img src={normal} alt="Normal" />
-                        <img src={mal} alt="Mal" />
-                    </div>
-                    <p>¿Por qué te sentiste así?</p>
-                    <textarea name="text" id="text"></textarea>
-                    <div className="feedBackButtons">
-                        <button>Cancelar</button>
-                        <button>Confirmar</button>
-                    </div>
-                </section>
-            </div>
+                        
+                    ) }
+                </div>
+            )) }
             
             { isPopUpVisible && selectedExercise && <ViewPopUp name={selectedExercise} /> }
         </article>
